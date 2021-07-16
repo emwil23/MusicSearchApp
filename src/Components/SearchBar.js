31a3ee7316163d5c1a6fbf940ab1da8184fbf039
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-07-16 13:34:32 +0530

    .

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [term, setTerm] = useState("");
+
+  return (
+    <div className="ui segment">
+      <form className="ui form">
+        <div className="field">
+          <label>Search</label>
+          <input
+            type="text"
+            placeholder="Search for songs here"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
